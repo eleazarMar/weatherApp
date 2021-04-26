306ec7af55426e993c200a6ee31f4bf5a23ad5ce
kompi18 <72274105+kompi18@users.noreply.github.com>
2021-04-26 12:30:06 -0500

    Initial Commit

---
+++ WeatherForecastPM/WeatherForecastPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastPMApp.swift
+//  WeatherForecastPM
+//
+//  Created by P.M. Class on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
